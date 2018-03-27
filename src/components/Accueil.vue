525f1111c200cfa4ca419835bd56b9f889bd72ad
Benjamin FRIZON <benjamin.frizon@ebp.local>
2018-03-27 11:07:38 +0200

    '............'

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,6 +1,32 @@
 <template>
+  <ul class="accueil">
+    <li><i class="material-icons">add</i></li>
+    <li><i class="material-icons">add</i></li>
+    <li><i class="material-icons">add</i></li>
+  </ul>
 
 </template>
+
+<style lang="sass">
+.accueil
+
+  li
+    background: #193f5e
+    width: 60px
+    height: 60px
+    color: #ffffff
+    border-radius: 50%
+    display: flex
+    align-items: center
+    margin: 1rem
+
+    .material-icons
+      text-align: center
+      flex: 1
+
+
+
+</style>
 
 <script>
 export default {
